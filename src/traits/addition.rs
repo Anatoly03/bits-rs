257abe9d7b284f7db5082f3916bb80d598dde6ef
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-05 13:22:05 +0100

    impl add

--- src/traits/addition.rs
+++ src/traits/addition.rs
@@ -1,12 +1,35 @@
 use std::ops::Add;
 
-use crate::structs::UBigInt;
+use crate::structs::*;
 
-// impl Add for UBigInt {
-//     type Output = UBigInt;
+impl Add for UBigInt {
+    type Output;
 
-//     fn add(self, rhs: Self) -> Self::Output {
-//         println!("{}, {}", self, rhs);
-//         self.clone()
-//     }
-// }
+    fn add(self, rhs: Self) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Add for BigInt {
+    type Output;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Add for UFraction {
+    type Output;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Add for Fraction {
+    type Output;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        todo!()
+    }
+}
