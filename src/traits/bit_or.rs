df81fa2901876af9ae4332c784940d558727cc6e
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-05 15:40:45 +0100

    impl bit or

---
+++ src/traits/bit_or.rs
@@ -0,0 +1,36 @@
+use std::cmp::*;
+use std::ops::BitOr;
+
+use crate::structs::nums::*;
+
+/**
+ * UBigInt + UBigInt
+ */
+impl<'a, 'b> BitOr<&'b UBigInt> for &'a UBigInt {
+    type Output = UBigInt;
+
+    fn bitor(self, rhs: &'b UBigInt) -> Self::Output {
+        let mut result = Vec::new();
+        let limit = max(self.0.len(), rhs.0.len());
+
+        for i in 0..limit {
+            let a1 = self.0.get(i).or(Some(&0)).unwrap();
+            let a2 = rhs.0.get(i).or(Some(&0)).unwrap();
+
+            result.push(a1 | a2);
+        }
+
+        UBigInt(result)
+    }
+}
+
+/**
+ * BigInt + BigInt
+ */
+impl<'a, 'b> BitOr<&'b BigInt> for &'a BigInt {
+    type Output = BigInt;
+
+    fn bitor(self, rhs: &'b BigInt) -> Self::Output {
+        BigInt (self.0 == rhs.0, &self.1 | &rhs.1)
+    }
+}
--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,6 +1,7 @@
 
 pub mod add;
 pub mod bit_and;
+pub mod bit_or;
 pub mod clone;
 pub mod debug;
 pub mod neg;
