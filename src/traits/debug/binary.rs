5bdb0c8786e33b1aab74b464e97a980f0aea3bfa
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-05 14:51:29 +0100

    impl binary

---
+++ src/traits/debug/binary.rs
@@ -0,0 +1,37 @@
+use std::fmt::Binary;
+
+use crate::structs::nums::*;
+
+impl Binary for UBigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let bits = self
+            .0
+            .clone()
+            .into_iter()
+            .rev()
+            .map(|i| format!("{i:b}"))
+            .reduce(|a, b| a + &b)
+            .or(Some("0".to_string()))
+            .unwrap();
+
+        f.debug_tuple("UBigInt").field(&bits).finish()
+    }
+}
+
+impl Binary for BigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl Binary for UFraction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl Binary for Fraction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
