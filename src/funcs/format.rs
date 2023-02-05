7cee703370ac7804315d510ae404c51b843df45e
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-05 20:58:57 +0100

    fix bug where 0 would not print

--- src/funcs/format.rs
+++ src/funcs/format.rs
@@ -1,44 +1,76 @@
-use crate::structs::nums::*;
 use crate::structs::consts::UNIT_SIZE;
+use crate::structs::nums::*;
 
 pub fn pad_left(bn: String, length: usize) -> String {
     String::from("0").repeat(length - bn.len()) + &bn
 }
 
+pub fn unpad_left(mut num: String) -> String {
+    while let Some(i) = num.get(0..1) {
+        if i == "0" {
+            num.remove(0);
+            continue;
+        }
+        break;
+    }
+    num
+}
+
 impl UBigInt {
     pub fn to_bin(&self) -> String {
-        self.0
+        let s = self
+            .0
             .clone()
             .into_iter()
             .rev()
             .map(|i| format!("{i:b}"))
             .map(|i| pad_left(i, UNIT_SIZE))
             .reduce(|a, b| a + &b)
+            .map(unpad_left)
             .or(Some("0".to_string()))
-            .unwrap()
+            .unwrap();
+
+        if s.is_empty() {
+            return String::from("0");
+        }
+        s
     }
 
     pub fn to_octal(&self) -> String {
-        self.0
+        let s = self
+            .0
             .clone()
             .into_iter()
             .rev()
             .map(|i| format!("{i:o}"))
             .map(|i| pad_left(i, UNIT_SIZE / 2))
             .reduce(|a, b| a + &b)
+            .map(unpad_left)
             .or(Some("0".to_string()))
-            .unwrap()
+            .unwrap();
+
+        if s.is_empty() {
+            return String::from("0");
+        }
+        s
     }
 
     pub fn to_hexadecimal(&self) -> String {
-        self.0
+        let s = self
+            .0
             .clone()
             .into_iter()
             .rev()
             .map(|i| format!("{i:X}"))
             .map(|i| pad_left(i, UNIT_SIZE / 4))
             .reduce(|a, b| a + &b)
+            .map(unpad_left)
             .or(Some("0".to_string()))
-            .unwrap()
+            .unwrap();
+
+        if s.is_empty() {
+            return String::from("0");
+        }
+        s
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ pub mod traits;
 fn main() {
     let big_int = UBigInt (vec![1, 10]);
     let big_int_2 = UBigInt (vec![Atomic::MAX, 10]);
-    let big_zero = UBigInt (vec![255]);
+    let big_zero = UBigInt (vec![0]);
     let sum = &big_int + &big_int_2;
     
     println!("{:b}", big_int);
