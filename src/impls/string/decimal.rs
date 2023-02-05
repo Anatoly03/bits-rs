9a167ba1414a4f09c695f317cce8ef190fff46d7
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-05 21:09:20 +0100

    create space for implementing displa trait

---
+++ src/impls/string/decimal.rs
@@ -0,0 +1,15 @@
+use std::fmt::Display;
+
+use crate::structs::nums::*;
+
+impl Display for UBigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!("should convert to decimal")
+    }
+}
+
+impl Display for BigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!("should convert to decimal")
+    }
+}
--- src/impls/string/mod.rs
+++ src/impls/string/mod.rs
@@ -1,5 +1,5 @@
 pub mod binary;
 pub mod debug;
+pub mod decimal;
 pub mod hexadecimal;
 pub mod octal;
-pub mod string;
