86beb1a45c216411c8bc50b3bdeecd58b3e0e553
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-05 16:08:41 +0100

    move to math mod

---
+++ src/traits/math/mod.rs
@@ -0,0 +1,7 @@
+pub mod add;
+pub mod bit_and;
+pub mod bit_or;
+pub mod bit_xor;
+pub mod neg;
+pub mod shl;
+pub mod sub;
--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,12 +1,7 @@
 
-pub mod add;
-pub mod bit_and;
-pub mod bit_or;
-pub mod bit_xor;
-pub mod clone;
+pub mod math;
 pub mod debug;
-pub mod neg;
+
+pub mod clone;
 pub mod ord;
-pub mod shl;
 pub mod string;
-pub mod sub;
