f0b149115e4411c822a9b9ab68bf6e1a796fc9ce
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-02-05 14:03:48 +0100

    add debug format

---
+++ src/traits/debug.rs
@@ -0,0 +1,27 @@
+use std::fmt::Debug;
+
+use crate::structs::nums::*;
+
+impl Debug for UBigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("UBigInt").field(&self.0).finish()
+    }
+}
+
+impl Debug for BigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("BigInt").field(&self.0).field(&self.1).finish()
+    }
+}
+
+impl Debug for UFraction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("UFraction").field(&self.0).field(&self.1).finish()
+    }
+}
+
+impl Debug for Fraction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Fraction").field(&self.0).field(&self.1).finish()
+    }
+}
--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,5 +1,6 @@
 
 pub mod add;
 pub mod clone;
+pub mod debug;
 pub mod neg;
 pub mod string;
